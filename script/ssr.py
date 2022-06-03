36e645710472eb55ec50cbdbd824dc4faf87d447
Eastarpen <eaststarpen@gmail.com>
2022-06-03 12:17:25 +0800

    protocol structure

--- script/ssr.py
+++
@@ -1,3 +0,0 @@
-# ssr://server:port:protocol:method:obfs:password_base64/?params_base64
-
-# https://haleyryan.tumblr.com/post/178242254081/ssr%E9%85%B8%E9%85%B8%E4%B9%B3%E4%B8%8Ess%E5%B0%8F%E9%A3%9E%E6%9C%BA%E9%93%BE%E6%8E%A5%E5%8A%A0%E5%AF%86%E6%96%B9%E5%BC%8F%E7%9A%84%E6%8E%A2%E8%AE%A8
